685c5295163a703437e80f59db961cce6855794d
tzfqz <249042971@qq.com>
2019-09-18 00:06:28 +0800

    init1

---
+++ precache-manifest.597f3fbcbe0b29e83af9bcb30042fe01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b205c5e46363378d79d7e64a66f3d4e0",
+    "url": "/teach/index.html"
+  },
+  {
+    "revision": "3dd6230daced32e1acc2",
+    "url": "/teach/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "7774deaae6ca03e69bda",
+    "url": "/teach/static/js/2.2f955b3b.chunk.js"
+  },
+  {
+    "revision": "3dd6230daced32e1acc2",
+    "url": "/teach/static/js/main.c114c169.chunk.js"
+  },
+  {
+    "revision": "0c63388583a5bfccaf9e",
+    "url": "/teach/static/js/runtime~main.e09a9e94.js"
+  }
+]);
